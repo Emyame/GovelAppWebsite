ca056c3416be95b15c1b0b473e03eb2912d11d82
Mert <bjkmertyerlikaya@gmail.com>
2017-01-12 12:45:42 +0300

    Last version

--- countdown/js/simplyCountdown.js
+++ countdown/js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2017,
-                month: 3,
-                day: 1,
+                year: 2015,
+                month: 6,
+                day: 28,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
